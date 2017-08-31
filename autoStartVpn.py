b9e34acdcb4e223fc0bbfa2d057b42ace988c1ce
Johnny Anderson dos Santos <johnnyadsantos@gmail.com>
2017-08-30 22:24:11 -0300

    Added some beauty

--- autoStartVpn.py
+++ autoStartVpn.py
@@ -1,13 +1,16 @@
-import os
+#!/usr/bin/python3
+from os import system
 
 file = open("vpn-id.txt")
-uuid = file.readline()
+uuid = file.readline().strip()
 file.close()
 
 print("Starting VPN '" + uuid + "'...")
-output = os.system("nmcli con up " + uuid)
+output = system("nmcli con up " + uuid)
 
 if output == 0:
     print("Connected.")
+    exit(0)
 else:
     print("Something went wrong. :(")
+    exit(1)
