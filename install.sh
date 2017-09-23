c62c10e4b9ecf79ea5f4825d42d4e0c4434ad588
Johnny Santos <johnnyadsantos@gmail.com>
2017-09-23 01:13:33 -0300

    Add installation for fonts and powerline

--- install.sh
+++ install.sh
@@ -10,3 +10,35 @@ do
 done
 
 rm -f ~/.gitignore
+
+echo "Installing fonts..."
+
+mkdir -m=755 ~/.local/share/fonts > /dev/null
+cd ~/.local/share/fonts
+
+# Powerline font
+curl -O https://raw.githubusercontent.com/powerline/powerline/develop/font/PowerlineSymbols.otf
+
+# Firacode
+
+curl -O https://raw.githubusercontent.com/tonsky/FiraCode/master/distr/otf/FiraCode-Bold.otf
+curl -O https://raw.githubusercontent.com/tonsky/FiraCode/master/distr/otf/FiraCode-Light.otf
+curl -O https://raw.githubusercontent.com/tonsky/FiraCode/master/distr/otf/FiraCode-Medium.otf
+curl -O https://raw.githubusercontent.com/tonsky/FiraCode/master/distr/otf/FiraCode-Regular.otf
+curl -O https://raw.githubusercontent.com/tonsky/FiraCode/master/distr/otf/FiraCode-Retina.otf
+
+# Change the font files permissions
+for f in ./*.otf
+do
+    [ -f "$f" ] && chmod 755 $f
+done
+
+echo "Rebuilding font cache..."
+fc-cache -f -v
+
+cd $BASEDIR
+
+echo "Installing powerline..."
+sudo pip install powerline-status
+
+
