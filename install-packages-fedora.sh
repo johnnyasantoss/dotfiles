99ce63b527cd7d7a6b205a8ab1cadbee6d18bcdc
Johnny Santos <johnnyadsantos@gmail.com>
2017-10-20 21:57:43 -0200

    Add vscode to fedora install script

--- install-packages-fedora.sh
+++ install-packages-fedora.sh
@@ -2,6 +2,10 @@
 
 echo "Adding repos..."
 
+# VsCode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 
@@ -22,5 +26,6 @@ sudo dnf -y install \
 	yarn \
 	gcc-c++ \
 	make \
+	code
 
 
