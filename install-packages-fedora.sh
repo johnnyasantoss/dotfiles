0d99165c393ddbb6af9d93fbf6853b631b1447db
Johnny Santos <johnnyadsantos@gmail.com>
2017-10-21 00:33:34 -0200

    Add more needed software to fedora install script

--- install-packages-fedora.sh
+++ install-packages-fedora.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
 
 echo "Adding repos..."
+# Docker
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+	    --add-repo \
+	        https://download.docker.com/linux/fedora/docker-ce.repo
 
+# Yarn
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 
+# Nodejs
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 
 echo "Installing packages..."
 
@@ -22,5 +29,10 @@ sudo dnf -y install \
 	yarn \
 	gcc-c++ \
 	make \
+	mono-devel \
+	gnome-tweak-tool \
+	gstreamer \
+	gstreamer-plugins-good \
+	gstreamer-plugins-bad
 
 
