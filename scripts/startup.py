2ae04cde4c0e9dc4082acf23d3283ffb6c1a3e7f
Johnny Santos <johnnyadsantos@gmail.com>
2019-01-27 22:12:43 +0100

    Remove unused scripts

--- scripts/startup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from glob import glob
-from os import system
-
-for script in glob("./*.py"):
-    if "startup.py" in script:
-        continue
-    print("Running " + script + "...")
-    system("python3 " + script)
